b6eea49055062def53c6e852a76988919552a835
Sergey Mudrik <sergey.mudrik@gmail.com>
2013-09-15 20:44:25 +0300

    added python3

---
+++ fact-python3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+times = 1000000
+fact_16 = 20922789888000
+
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+
+ok = True
+for i in range(times):
+    ok = ok and fact(16) == fact_16
+
+print("python3 finish %i - %s" % (times, 'ok' if ok else 'fail'))
