e82e28a48fd2d23d033edd7d6d246df8ea734aaf
Sergey Mudrik <sergey.mudrik@gmail.com>
2013-09-15 21:27:26 +0300

    added Dart

---
+++ fact.dart
@@ -0,0 +1,20 @@
+#!/usr/bin/env dart
+
+const times = 200000000;
+const fact_16 = 20922789888000;
+
+int fact(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+
+void main() {
+    bool ok = true;
+    for (int i = 0; i < times; i++) {
+        ok = ok && fact(16) == fact_16;
+    }
+
+    print("dart finish ${times.toString()} - ${ok ? 'ok' : 'fail'}");
+}
