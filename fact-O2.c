a565ad1c0fbd26a3faa84db1b79d568fc9b1b1df
Sergey Mudrik <sergey.mudrik@gmail.com>
2013-09-28 13:49:41 +0300

    fixed times constant for new LLVM version 5.0 (clang-500.2.76)

--- fact-O2.c
+++ fact-O2.c
@@ -1,1 +1,19 @@
-fact.c
+#include <stdio.h>
+
+#define times 9000000000
+#define fact_16 20922789888000
+
+long fact(int n) {
+    if (n == 1)
+        return 1;
+    return(n * fact(n - 1));
+}
+
+int main() {
+    int ok = 1;
+    long i;
+    for(i = 0; i < times; ++i) {
+        ok = ok && fact(16) == fact_16;
+    }
+    printf("C finish %li - %s\n", times, ok ? "ok" : "fail");
+}
