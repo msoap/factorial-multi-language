526bcde0adc7c8894c959cc1551d304e8945042c
Sergey Mudrik <sergey.mudrik@gmail.com>
2013-10-13 20:39:21 +0300

    new syntax in Rust

--- fact.rs
+++ fact.rs
@@ -6,8 +6,8 @@
     @@@ is_fast: 1
 */
 
-static times: uint = 50000000;
-static fact_16: uint = 20922789888000;
+static times: uint = 50_000_000;
+static fact_16: uint = 20_922_789_888_000;
 
 // ----------------------------------------------
 fn fact(n: uint) -> uint {
@@ -22,9 +22,9 @@ fn main() {
     let mut ok: bool = true;
     let mut i = 0;
 
-    while i < times {
+    while i <= times {
         ok = ok && fact(16) == fact_16;
-        i = i + 1;
+        i += 1;
     }
 
     println(fmt!("Rust finish %d - %s", times as int, if ok {"ok"} else {"fail"}));
