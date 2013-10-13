49533a129682bb1f25fad297eb48f82847bb6419
Sergey Mudrik <sergey.mudrik@gmail.com>
2013-10-13 11:24:52 +0300

    slow variant, but more haskell way

--- fact.hs
+++ fact.hs
@@ -6,31 +6,15 @@
 
 module Main where
 
-times = 100000000
+times = 10000000
 fact_16 = 20922789888000
-
-loop_code 0 _ = return ()
-loop_code n action = do
-    action
-    loop_code (n - 1) action
 
 factorial :: Integer -> Integer
 factorial 0 = 1
 factorial n = n * factorial(n - 1)
 
-fact_16_exec = do
-    let result = factorial(16) == fact_16
-    return result
-
 main = do
-    loop_code times fact_16_exec
-    putStrLn("Haskell finish " ++ (show times) ++ " - ok")
-
-    -- error :(
-    --   Stack space overflow: current size 8388608 bytes.
-    --   Use `+RTS -Ksize -RTS' to increase it.
-    --
-    -- let result = if foldl1 (&&) [factorial(16) == 20922789888000 | x <- [1 .. times]]
-    --              then "ok"
-    --              else "fail"
-    -- putStrLn("Haskell finish " ++ (show times) ++ " - " ++ result)
+    let result = if foldr1 (&&) [factorial(16) == 20922789888000 | x <- [1 .. times]]
+                 then "ok"
+                 else "fail"
+    putStrLn("Haskell finish " ++ (show times) ++ " - " ++ result)
