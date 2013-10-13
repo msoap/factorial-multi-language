17452725a9b7bf0f057ff9ab9184a4bf0cab9ffb
Sergey Mudrik <sergey.mudrik@gmail.com>
2013-10-13 12:49:43 +0300

    changed for more perl6 way

--- fact-perl6.pl
+++ fact-perl6.pl
@@ -4,14 +4,13 @@
 # @@@ skip_chart: 1
 
 my Int $times = 5_000;
-my Int $fact_16 = 20922789888000;
+my Int $fact_16 = 20_922_789_888_000;
 
 # ------------------------------------------------------------------------------
-sub fact(Int $n) {
-    return 1 if $n == 1;
-    return $n * fact($n - 1);
+sub postfix:<!>(Int $n) returns Int {
+    return [*] 1 .. $n;
 }
 
 my Bool $ok = True;
-$ok &&= fact(16) == $fact_16 for 1 .. $times;
+$ok &&= 16! == $fact_16 for 1 .. $times;
 say("perl6 finish $times - " ~ ($ok ?? 'ok' !! 'fail'));
