a0155bf53e459200ec6cca7cebb6d5e7ca6e94ac
Sergey Mudrik <sergey.mudrik@gmail.com>
2014-01-26 11:04:41 +0200

    added PyPy script

---
+++ fact-pypy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env pypy
+# -*- coding: utf8 -*-
+
+# @@@ get_version: pypy --version 2>&1 | awk 'NR == 2 {print $2}'
+# @@@ is_fast: 1
+# @@@ skip_chart: 1
+
+times = 1000000000
+fact_16 = 20922789888000
+
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+
+ok = True
+for i in xrange(times):
+    ok = ok and fact(16) == fact_16
+
+print "PyPy finish %i - %s" % (times, 'ok' if ok else 'fail')
