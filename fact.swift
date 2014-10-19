345e3805edc58e6a1dabcd2dec42e7c641a106a0
Sergey Mudrik <sergey.mudrik@gmail.com>
2014-10-19 10:53:28 +0300

    compile with optimizations

--- fact.swift
+++ fact.swift
@@ -1,22 +1,26 @@
 // @@@ get_version: swiftc --version | awk 'NR == 1 {print $3}'
-// @@@ before: swiftc fact.swift
+// @@@ before: swiftc -O fact.swift
 // @@@ instead: ./fact
 // @@@ after: rm fact
 // @@@ is_fast: 1
 
-let times = 30000000
-let fact_16 = 20922789888000
+let times = 200000000
+let fact_16: Int64 = 20922789888000
 
-func fact (n: Int) -> Int {
+func fact(n: Int64) -> Int64 {
     if n == 1 {
         return 1
     }
     return n * fact(n - 1)
 }
 
-var ok = true
-for i in 1...times {
-    ok = ok && fact(16) == fact_16
+func main() {
+    var ok = true
+    for i in 1...times {
+        ok = ok && fact(16) == fact_16
+    }
+
+    println("Swift finish " + String(times) + " - " + (ok ? "ok" : "fail"))
 }
 
-println("Swift finish " + String(times) + " - " + (ok ? "ok" : "fail"))
+main()
