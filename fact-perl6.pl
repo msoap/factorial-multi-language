92d31818c5265bd133c422729570255529fe90ac
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-02-15 21:58:31 +0200

    changed perl6 fact function for faster variant

--- fact-perl6.pl
+++ fact-perl6.pl
@@ -1,16 +1,24 @@
 #!/usr/bin/env perl6
 
-# @@@ get_version: perl6 -v 2>&1 | perl -pE 's/This \s+ is \s+ perl6 \s+ version \s+//x'
+# @@@ get_version: perl6 -v | perl -pE 's/This \s+ is \s+ perl6 \s+ version \s+//x'
 # @@@ skip_chart: 1
 
-my Int $times = 5_000;
+my Int $times = 300_000;
 my Int $fact_16 = 20_922_789_888_000;
 
 # ------------------------------------------------------------------------------
+# example nice factorial function
+#   say 16!
 sub postfix:<!>(Int $n) returns Int {
     return [*] 1 .. $n;
 }
 
+# ------------------------------------------------------------------------------
+sub fact(Int $n) returns Int {
+    return 1 if $n == 1;
+    return $n * fact($n - 1);
+}
+
 my Bool $ok = True;
-$ok &&= 16! == $fact_16 for 1 .. $times;
+$ok &&= fact(16) == $fact_16 for 1 .. $times;
 say("perl6 finish $times - " ~ ($ok ?? 'ok' !! 'fail'));
