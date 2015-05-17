6cd4145353314b1d8afbd7953d364dd4b60f29d6
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-05-17 19:28:00 +0300

    Added nim language (nim-lang.org)

---
+++ fact.nim
@@ -0,0 +1,25 @@
+#  @@@ get_version: nim --version | awk 'NR == 1 {print $4}'
+#  @@@ before: nim c fact.nim
+#  @@@ instead: ./fact
+#  @@@ after: rm -rf fact nimcache
+
+# with:
+#   nim c -d:release fact.nim
+# programm fast with any "times"
+
+const times = 10_000_000
+const fact_16 = 20_922_789_888_000
+
+proc fact(n: int): int =
+  if n == 1:
+      1
+  else:
+      n * fact(n - 1)
+
+proc main() =
+    var ok = true
+    for i in 0..times:
+        ok = ok and fact(16) == fact_16
+    echo("nim finish ", times, " - ", if ok: "ok" else: "fail");
+
+main()
