53bf2325e6f8b92ee10e960f683b718b3f28cd8e
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-06-27 11:17:09 +0300

    New: Rust, Dart, PyPy

--- fact-jsc.js
+++ fact-jsc.js
@@ -2,7 +2,7 @@
 // ln -s /System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc ~/bin/
 
 /*
-    @@@ version: 8.0.5
+    @@@ version: 8.0.6
     @@@ instead: jsc fact-jsc.js
     @@@ is_fast: 1
 */
