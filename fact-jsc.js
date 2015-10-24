0a97b0d16ebcaf47535793598a3a3e9f4da28d1d
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-10-24 20:40:48 +0300

    New versions

--- fact-jsc.js
+++ fact-jsc.js
@@ -2,7 +2,7 @@
 // ln -s /System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc ~/bin/
 
 /*
-    @@@ version: 8.0.8
+    @@@ version: 9.0
     @@@ instead: jsc fact-jsc.js
     @@@ is_fast: 1
 */
