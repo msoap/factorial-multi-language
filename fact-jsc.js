76fba50c7eb1b2e51dad2dfdb4c6da9c5f96c82b
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-10-30 21:48:04 +0200

    New safari

--- fact-jsc.js
+++ fact-jsc.js
@@ -2,7 +2,7 @@
 // ln -s /System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc ~/bin/
 
 /*
-    @@@ version: 9.0
+    @@@ version: 9.0.1
     @@@ instead: jsc fact-jsc.js
     @@@ is_fast: 1
 */
