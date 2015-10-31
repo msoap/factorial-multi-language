636d2dfd0257452e115def4178505ad11bf74dd1
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-10-31 16:41:26 +0200

    gofmt for fact.go

--- fact.go
+++ fact.go
@@ -1,39 +1,39 @@
 package main
 
 /*
-    @@@ get_version: go version | awk '{print $3}' | sed 's/go//'
-    @@@ before: go build fact.go
-    @@@ instead: ./fact
-    @@@ after: rm fact
-    @@@ is_fast: 1
+   @@@ get_version: go version | awk '{print $3}' | sed 's/go//'
+   @@@ before: go build fact.go
+   @@@ instead: ./fact
+   @@@ after: rm fact
+   @@@ is_fast: 1
 */
 
 import "fmt"
 
 const (
-    times = 150000000
-    fact_16 uint64 = 20922789888000
+	times          = 150000000
+	fact_16 uint64 = 20922789888000
 )
 
 // ----------------------------------------------
 func fact(n uint64) uint64 {
-    if n == 1 {
-        return 1
-    }
-    return n * fact(n - 1)
+	if n == 1 {
+		return 1
+	}
+	return n * fact(n-1)
 }
 
 // ----------------------------------------------
 func main() {
-    ok := true
+	ok := true
 
-    for i := 0; i < times; i++ {
-        ok = ok && fact(16) == fact_16
-    }
+	for i := 0; i < times; i++ {
+		ok = ok && fact(16) == fact_16
+	}
 
-    if ok {
-        fmt.Printf("Go finish %d - ok\n", times)
-    } else {
-        fmt.Printf("Go finish %d - fail\n", times)
-    }
+	if ok {
+		fmt.Printf("Go finish %d - ok\n", times)
+	} else {
+		fmt.Printf("Go finish %d - fail\n", times)
+	}
 }
