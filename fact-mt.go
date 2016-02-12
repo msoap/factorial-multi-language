1b36448aae462c34648aed9a32a0c1a744553795
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-02-13 00:16:11 +0200

    Added Go multi-threaded version

---
+++ fact-mt.go
@@ -0,0 +1,56 @@
+package main
+
+/*
+   @@@ get_version: go version | awk '{print $3}' | sed 's/go//'
+   @@@ before: go build fact-mt.go
+   @@@ instead: ./fact-mt
+   @@@ after: rm ./fact-mt
+   @@@ is_fast: 1
+*/
+
+import (
+	"fmt"
+	"runtime"
+)
+
+const (
+	times          = 150000000
+	fact16  uint64 = 20922789888000
+	threads        = 4
+)
+
+// ----------------------------------------------
+func fact(n uint64) uint64 {
+	if n == 1 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+// ----------------------------------------------
+func main() {
+	runtime.GOMAXPROCS(threads)
+	ok := true
+	ch := make(chan bool)
+
+	for k := 0; k < threads; k++ {
+		go func() {
+			okTh := true
+			for i := 0; i < times/threads; i++ {
+				okTh = okTh && fact(16) == fact16
+			}
+
+			ch <- okTh
+		}()
+	}
+
+	for k := 0; k < threads; k++ {
+		ok = ok && <-ch
+	}
+
+	if ok {
+		fmt.Printf("Go-mt finish %d - ok\n", times)
+	} else {
+		fmt.Printf("Go-mt finish %d - fail\n", times)
+	}
+}
