cd375a4ca0b46a0f38e4d1f70d6f4a48c134e60b
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-03-28 00:18:50 +0300

    Added Kotlin

--- fact.kt
+++ fact.kt
@@ -1,22 +1,30 @@
-package fact
- 
 /*
 before run need exec:
     docker-machine start; eval "$(docker-machine env default)"
 
     @@@ get_version: docker run --rm supermina999/alpine-kotlin kotlinc -version 2>&1 | awk '{print $5}'
     @@@ before: docker run --rm -v "$PWD":/app -w /app supermina999/alpine-kotlin kotlinc fact.kt
-    @@@ instead: docker run --rm -v "$PWD":/app -w /app supermina999/alpine-kotlin kotlin fact
-    @@@ after: rm fact.class
+    @@@ instead: docker run --rm -v "$PWD":/app -w /app supermina999/alpine-kotlin kotlin FactKt
+    @@@ after: rm -rf FactKt.class ./META-INF
     @@@ is_fast: 1
 */
 
-fun fact(n: Int): Int {
-    if (n == 1)
-        return 1;
-    return n * fact(n - 1);
+private val times = 200000000
+private val fact16 = 20922789888000L
+
+fun fact(n: Long): Long {
+    if (n == 1L)
+        return 1
+    return n * fact(n - 1)
 }
  
 fun main(args: Array<String>) {
-    println("fact", fact(16))
+    var ok: Boolean = true
+
+    for (i: Int in 1..times) {
+        ok = ok && fact(16) == fact16
+    }
+
+    val result = if (ok) "ok" else "fail"
+    println("Kotlin finish $times - $result")
 }
