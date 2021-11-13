6f23e1bd9262c0e9a55f51896176267e0fbc792b
Sergey Mudrik <sergey.mudrik@gmail.com>
2021-11-14 00:08:11 +0200

    New versions

--- fact.kt
+++ fact.kt
@@ -1,7 +1,7 @@
 /*
-    @@@ get_version: docker run --rm zenika/alpine-kotlin kotlinc -version 2>&1 | awk '{print $3}'
-    @@@ before: docker run --rm -v "$PWD":/app -w /app zenika/alpine-kotlin kotlinc fact.kt
-    @@@ instead: docker run --rm -v "$PWD":/app -w /app zenika/alpine-kotlin kotlin FactKt
+    @@@ get_version: docker run --rm zenika/kotlin kotlinc -version 2>&1 | awk '{print $3}'
+    @@@ before: docker run --rm -v "$PWD":/app -w /app zenika/kotlin kotlinc fact.kt
+    @@@ instead: docker run --rm -v "$PWD":/app -w /app zenika/kotlin kotlin FactKt
     @@@ after: rm -rf FactKt.class ./META-INF
     @@@ is_fast: 1
 */
@@ -15,7 +15,7 @@ fun fact(n: Long): Long {
     return n * fact(n - 1)
 }
 
-fun main(args: Array<String>) {
+fun main() {
     var ok: Boolean = true
 
     for (i: Int in 1..times) {
