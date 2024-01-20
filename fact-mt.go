8a6b8e2bf986beee2b7f7b1b429900e289dfc2b1
Serhii Mudryk <sergey.mudrik@gmail.com>
2024-01-20 16:05:49 +0200

    improved fact-mt.go for single run

--- fact-mt.go
+++ fact-mt.go
@@ -11,12 +11,15 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
+	"time"
 )
 
 const (
-	times          = 150000000
-	fact16  uint64 = 20922789888000
-	threads        = 8
+	times             = 1000000000
+	fact16     uint64 = 20922789888000
+	defThreads        = 8
 )
 
 // ----------------------------------------------
@@ -29,8 +32,14 @@ func fact(n uint64) uint64 {
 
 // ----------------------------------------------
 func main() {
+	startedAt := time.Now()
 	ok := true
 	ch := make(chan bool)
+
+	threads := runtime.NumCPU()
+	if threads == 0 {
+		threads = defThreads
+	}
 
 	for k := 0; k < threads; k++ {
 		go func() {
@@ -47,9 +56,14 @@ func main() {
 		ok = ok && <-ch
 	}
 
+	rps := float64(times) / time.Since(startedAt).Seconds()
 	if ok {
 		fmt.Printf("Go-mt finish %d - ok\n", times)
 	} else {
 		fmt.Printf("Go-mt finish %d - fail\n", times)
+	}
+
+	if len(os.Args) > 1 && os.Args[1] == "rps" {
+		fmt.Printf("%0.1f rps, %s in %d threads\n", rps, time.Since(startedAt).Round(time.Millisecond), threads)
 	}
 }
